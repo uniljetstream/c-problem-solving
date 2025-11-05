15900fd5df9d7bc47a0b7f3bf818d29c1dc62c64
uniljetstream <153919912+uniljetstream@users.noreply.github.com>
2025-11-05 22:05:28 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int t, a, b;
+    
+    scanf("%d", &t);
+    
+    for(int i=1;i<t+1;i++)
+    {
+        scanf("%2d %2d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i, a, b, a+b);
+    }
+    
+    return 0;
+}
