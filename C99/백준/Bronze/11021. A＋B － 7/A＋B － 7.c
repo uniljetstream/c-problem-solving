68889a45eb8580a87e68553f754b6aad18dcdc70
uniljetstream <153919912+uniljetstream@users.noreply.github.com>
2025-11-05 22:00:51 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/11021. A＋B － 7/A＋B － 7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int t, a, b;
+    
+    scanf("%d", &t);
+    
+    for(int i=1;i<t+1;i++)
+    {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d\n", i, a+b);
+    }
+    
+    return 0;
+}
