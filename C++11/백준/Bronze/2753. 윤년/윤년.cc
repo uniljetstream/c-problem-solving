31d76cc15b6006b313bec0232cc11ff132ca97c0
uniljetstream <153919912+uniljetstream@users.noreply.github.com>
2025-11-15 00:36:50 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int year;
+    
+    cin >> year;
+    
+    if(year%4==0 and (year%100 != 0 or year%400==0))
+        cout << '1';
+    else cout << '0';
+    
+    return 0;
+}
