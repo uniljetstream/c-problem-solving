717d869efe62099a0bdd4fbc49082b928f192c70
uniljetstream <153919912+uniljetstream@users.noreply.github.com>
2025-11-16 01:17:35 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    
+    cin >> x >> y;
+    
+    if(x > 0 and y > 0) printf("1");
+    else if(x < 0 and y > 0) printf("2");
+    else if(x < 0 and y < 0) printf("3");
+    else if(x > 0 and y < 0) printf("4");
+    
+    return 0;
+}
